93266d46457a5f9772c742606a4a4cdd70fb2653
mredaeljarib <35342659+mredaeljarib@users.noreply.github.com>
2018-06-08 18:34:52 +0200

    Update ApiManager.java
    commit hecho

--- Ordinaria_Entornos/src/ordinaria_entornos/ApiManager.java
+++ Ordinaria_Entornos/src/ordinaria_entornos/ApiManager.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ordinaria_entornos;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-/**
- *
- * @author mohbre
- */
 public class ApiManager​ {
-    
+
+	public double averiguaCambio(String divisaOrigen, String divisaDestino) {
+
+		String json = "";
+
+		String url = "http://free.currencyconverterapi.com/api/v5/convert?q=" + divisaOrigen + "_" + divisaDestino
+				+ "&compact=y";
+
+		json = MiClienteREST.request(url);
+
+		try {
+			JSONObject json2 = new JSONObject(json);
+			JSONObject json3 = json2.getJSONObject(divisaOrigen + "_" + divisaDestino);
+
+			Double d = json3.getDouble("val");
+
+			return d;
+
+		} catch (JSONException e) {
+			return 0;
+		}
+
+	}
+
 }
